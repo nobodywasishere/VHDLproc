d6954ed380d06e04cc376dfb21e0998d70492950
Margret Riegert <margret@eowyn.net>
2022-05-12 02:24:19 -0400

    Black formatting

--- setup.py
+++ setup.py
@@ -2,25 +2,24 @@ from setuptools import setup
 from vhdlproc import vhdlproc
 
 setup(
-    name='vhdlproc',
-    version=vhdlproc.__version__,    
-    description='A simple VHDL preprocessor',
-    url='https://github.com/nobodywasishere/vhdlproc',
-    author='Margret Riegert',
-    author_email='margret@eowyn.net',
-    license='GPLv3',
-    packages=['vhdlproc'],
-    package_data={'vhdlproc': ['tests/*']},
-    install_requires=['argparse'],
-
+    name="vhdlproc",
+    version=vhdlproc.__version__,
+    description="A simple VHDL preprocessor",
+    url="https://github.com/nobodywasishere/vhdlproc",
+    author="Margret Riegert",
+    author_email="margret@eowyn.net",
+    license="GPLv3",
+    packages=["vhdlproc"],
+    package_data={"vhdlproc": ["tests/*"]},
+    install_requires=["argparse", "black"],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent'
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
     ],
     entry_points={"console_scripts": ["vhdlproc = vhdlproc.vhdlproc:_cli"]},
 )
--- vhdlproc/vhdlproc.py
+++ vhdlproc/vhdlproc.py
@@ -16,73 +16,104 @@ __version__ = "v2.1"
 class Infix:
     def __init__(self, function):
         self.function = function
+
     def __or__(self, other):
         return self.function(other)
+
     def __ror__(self, other):
         return Infix(lambda x, self=self, other=other: self.function(other, x))
+
     def __truediv__(self, other):
         return self.function(other)
+
     def __rtruediv__(self, other):
         return Infix(lambda x, self=self, other=other: self.function(other, x))
+
     def __call__(self, value1, value2):
         return self.function(value1, value2)
 
+
 # functions for implementing VHDL operators in python for evaluating statements
 # relational
-r_eq   = Infix(lambda x,y:      x  == y)
-r_neq  = Infix(lambda x,y:      x  != y)
-r_lt   = Infix(lambda x,y:      x   < y)
-r_leq  = Infix(lambda x,y:      x  <= y)
-r_gt   = Infix(lambda x,y:      x   > y)
-r_geq  = Infix(lambda x,y:      x  >= y)
+r_eq = Infix(lambda x, y: x == y)
+r_neq = Infix(lambda x, y: x != y)
+r_lt = Infix(lambda x, y: x < y)
+r_leq = Infix(lambda x, y: x <= y)
+r_gt = Infix(lambda x, y: x > y)
+r_geq = Infix(lambda x, y: x >= y)
 # logical
-l_and  = Infix(lambda x,y:      x and y)
-l_or   = Infix(lambda x,y:      x  or y)
-l_nand = Infix(lambda x,y: not (x and y))
-l_nor  = Infix(lambda x,y: not (x  or y))
-l_xor  = Infix(lambda x,y:      x  != y)
-l_xnor = Infix(lambda x,y:      x  == y)
+l_and = Infix(lambda x, y: x and y)
+l_or = Infix(lambda x, y: x or y)
+l_nand = Infix(lambda x, y: not (x and y))
+l_nor = Infix(lambda x, y: not (x or y))
+l_xor = Infix(lambda x, y: x != y)
+l_xnor = Infix(lambda x, y: x == y)
 
 
 class VHDLproc:
-    _tool_name    = "VHDLproc"
-    _version      = __version__
-    _directives   = ['`warning', '`error', '`if', '`elsif', '`else', '`end', '`include', '`define']
+    _tool_name = "VHDLproc"
+    _version = __version__
+    _directives = [
+        "`warning",
+        "`error",
+        "`if",
+        "`elsif",
+        "`else",
+        "`end",
+        "`include",
+        "`define",
+    ]
     _comment_char = "-- "
 
     def _eval(self, statement: str, identifiers: Dict[str, str]):
 
         statement = statement.lower()
 
-        operators = ('=', '/=', '<', '<=', '>', '>=',
-                     'and', 'or', 'nand', 'nor', 'xor', 'xnor')
+        operators = (
+            "=",
+            "/=",
+            "<",
+            "<=",
+            ">",
+            ">=",
+            "and",
+            "or",
+            "nand",
+            "nor",
+            "xor",
+            "xnor",
+        )
 
-        for name in statement.replace('(', ' ').replace(')', ' ').split(' '):
-            if name not in identifiers and name not in operators and "'" not in name and '"' not in name:
-                logger.warning(f'Setting empty identifier {name}')
+        for name in statement.replace("(", " ").replace(")", " ").split(" "):
+            if (
+                name not in identifiers
+                and name not in operators
+                and "'" not in name
+                and '"' not in name
+            ):
+                logger.warning(f"Setting empty identifier {name}")
                 identifiers[name] = ""
-                
 
-        # replacing VHDL operators with Python ones to preserve VHDL 
+        # replacing VHDL operators with Python ones to preserve VHDL
         # operator precedence, / has higher precedence than |
-        statement = statement.replace(   ' = ',  ' /r_eq/ ')
-        statement = statement.replace(  ' /= ', ' /r_neq/ ')
-        statement = statement.replace(   ' < ',  ' /r_lt/ ')
-        statement = statement.replace(  ' <= ', ' /r_leq/ ')
-        statement = statement.replace(   ' > ',  ' /r_gt/ ')
-        statement = statement.replace(  ' >= ', ' /r_geq/ ')
+        statement = statement.replace(" = ", " /r_eq/ ")
+        statement = statement.replace(" /= ", " /r_neq/ ")
+        statement = statement.replace(" < ", " /r_lt/ ")
+        statement = statement.replace(" <= ", " /r_leq/ ")
+        statement = statement.replace(" > ", " /r_gt/ ")
+        statement = statement.replace(" >= ", " /r_geq/ ")
 
-        statement = statement.replace( ' and ', ' |l_and| ')
-        statement = statement.replace(  ' or ',  ' |l_or| ')
-        statement = statement.replace(' nand ',' |l_nand| ')
-        statement = statement.replace( ' nor ', ' |l_nor| ')
-        statement = statement.replace( ' xor ', ' |l_xor| ')
-        statement = statement.replace(' xnor ',' |l_xnor| ')
+        statement = statement.replace(" and ", " |l_and| ")
+        statement = statement.replace(" or ", " |l_or| ")
+        statement = statement.replace(" nand ", " |l_nand| ")
+        statement = statement.replace(" nor ", " |l_nor| ")
+        statement = statement.replace(" xor ", " |l_xor| ")
+        statement = statement.replace(" xnor ", " |l_xnor| ")
 
         for id in identifiers:
             locals()[id] = identifiers[id]
 
-        logger.debug(f'Evaluating statement {statement}: {eval(statement)}')
+        logger.debug(f"Evaluating statement {statement}: {eval(statement)}")
 
         return eval(statement)
 
@@ -100,11 +131,16 @@ class VHDLproc:
         with open(file) as f:
             code = f.read().splitlines()
 
-        include_path = '/'.join(file.split('/')[:-1]) + '/'
-        
+        include_path = "/".join(file.split("/")[:-1]) + "/"
+
         return self.parse(code, identifiers, include_path=include_path)
 
-    def parse(self, code: Union[List[str], str], identifiers: Dict[str, str] = {}, include_path: str = "./") -> str:
+    def parse(
+        self,
+        code: Union[List[str], str],
+        identifiers: Dict[str, str] = {},
+        include_path: str = "./",
+    ) -> str:
         """Steps through each line of the code, finds, and executes preprocessing statements, commenting them out once finished
 
         Args:
@@ -127,30 +163,32 @@ class VHDLproc:
         if isinstance(code, str):
             code = code.splitlines()
 
-        identifiers['TOOL_NAME']    = self._tool_name
-        identifiers['TOOL_VERSION'] = self._version
-        if 'VHDL_VERSION' not in identifiers:
-            identifiers['VHDL_VERSION'] = ""
-        if 'TOOL_TYPE' not in identifiers:
-            identifiers['TOOL_TYPE']    = ""
-        if 'TOOL_VENDOR' not in identifiers:
-            identifiers['TOOL_VENDOR']  = ""
-        if 'TOOL_EDITION' not in identifiers:
-            identifiers['TOOL_EDITION'] = ""
+        identifiers["TOOL_NAME"] = self._tool_name
+        identifiers["TOOL_VERSION"] = self._version
+        if "VHDL_VERSION" not in identifiers:
+            identifiers["VHDL_VERSION"] = ""
+        if "TOOL_TYPE" not in identifiers:
+            identifiers["TOOL_TYPE"] = ""
+        if "TOOL_VENDOR" not in identifiers:
+            identifiers["TOOL_VENDOR"] = ""
+        if "TOOL_EDITION" not in identifiers:
+            identifiers["TOOL_EDITION"] = ""
 
-        identifiers = {key.lower():value.lower() for (key,value) in identifiers.items()}
+        identifiers = {
+            key.lower(): value.lower() for (key, value) in identifiers.items()
+        }
 
         ifstack = [[True, False]]
         line_i = -1
 
         while line_i < len(code) - 1:
             line_i = line_i + 1
-            line = code[line_i].split('--')[0] # ignore comments
+            line = code[line_i].split("--")[0]  # ignore comments
 
-            if len(line.strip()) == 0: # skip empty lines
+            if len(line.strip()) == 0:  # skip empty lines
                 continue
 
-            if '`' == line.strip()[0]: # if this line is a directive
+            if "`" == line.strip()[0]:  # if this line is a directive
 
                 # https://stackoverflow.com/a/79985
                 # remove blank space and split words into a list
@@ -163,57 +201,69 @@ class VHDLproc:
                 if ifstack[-1][0]:
                     # if it's not a supported directive
                     if directive[0] not in self._directives:
-                        raise Exception(f"Line {line_i+1}: Unknown directive: {line.strip().split(' ')[0]}")
+                        raise Exception(
+                            f"Line {line_i+1}: Unknown directive: {line.strip().split(' ')[0]}"
+                        )
 
                     # print warning messages if not commented out
-                    elif directive[0] == '`warning':
+                    elif directive[0] == "`warning":
                         if len(directive) < 2:
-                            raise Exception(f'Line {line_i+1}: `warning directive requires a message')
+                            raise Exception(
+                                f"Line {line_i+1}: `warning directive requires a message"
+                            )
                         warning_message = directive[1][1:-1]
-                        logger.warning(f'Line {line_i+1}: {warning_message}')
+                        logger.warning(f"Line {line_i+1}: {warning_message}")
 
                     # print error messages if not commented out
-                    elif directive[0] == '`error':
+                    elif directive[0] == "`error":
                         if len(directive) < 2:
-                            raise Exception(f'Line {line_i+1}: `error directive requires a message')
+                            raise Exception(
+                                f"Line {line_i+1}: `error directive requires a message"
+                            )
                         error_message = directive[1][1:-1]
-                        logger.error(f'Line {line_i+1}: Error: {error_message}')
+                        logger.error(f"Line {line_i+1}: Error: {error_message}")
                         exit(1)
 
                     # open file and append source code at this location
-                    elif directive[0] == '`include':
+                    elif directive[0] == "`include":
                         filename = include_path + " ".join(directive[1:])[1:-1]
                         include = []
                         for incl_line in open(filename):
-                            include.append(incl_line.rstrip('\n'))
-                        code = code[:line_i+1] + include + code[line_i+1:]
+                            include.append(incl_line.rstrip("\n"))
+                        code = code[: line_i + 1] + include + code[line_i + 1 :]
 
-                    elif directive[0] == '`define':
+                    elif directive[0] == "`define":
                         if len(directive) != 3:
-                            raise Exception(f'Line {line_i+1}: `define directive requires a label and value')
+                            raise Exception(
+                                f"Line {line_i+1}: `define directive requires a label and value"
+                            )
                         identifiers[directive[1].lower()] = directive[2].lower()[1:-1]
 
-                if directive[0] == '`if':
+                if directive[0] == "`if":
                     if directive[-1] != "then":
-                        raise Exception(f'Line {line_i+1}: `if directive requires a then')
+                        raise Exception(
+                            f"Line {line_i+1}: `if directive requires a then"
+                        )
                     if ifstack[-1][0]:
-                        resp = self._eval(' '.join(directive[1:-1]), identifiers)
+                        resp = self._eval(" ".join(directive[1:-1]), identifiers)
                         ifstack.append([resp, resp])
                     else:
                         ifstack.append([False, True])
 
-                elif directive[0] == '`elsif':
+                elif directive[0] == "`elsif":
                     if directive[-1] != "then":
-                        raise Exception(f'Line {line_i+1}: `elsif directive requires a then')
-                    resp = self._eval(' '.join(directive[1:-1]), identifiers)
+                        raise Exception(
+                            f"Line {line_i+1}: `elsif directive requires a then"
+                        )
+                    resp = self._eval(" ".join(directive[1:-1]), identifiers)
                     ifstack[-1][0] = not ifstack[-1][1] and resp
                     ifstack[-1][1] = ifstack[-1][1] or resp
 
-                elif directive[0] == '`else':
+                elif directive[0] == "`else":
                     if ifstack[-2][0]:
                         ifstack[-1][0] = not ifstack[-1][1]
 
-                elif directive[0] == '`end':
+                elif directive[0] == "`end":
                     ifstack.pop()
 
             # don't comment out lines that are already commented out
@@ -225,9 +275,10 @@ class VHDLproc:
         if len(ifstack) > 1:
             raise Exception(f"Line {line_i+1}: Missing `end [ if ]")
 
-        return '\n'.join(code)
+        return "\n".join(code)
 
-def test_file(name: str, identifiers: Dict[str,str] = {}) -> bool:
+
+def test_file(name: str, identifiers: Dict[str, str] = {}) -> bool:
     """Runs a built-in test to verify functionality
 
     Args:
@@ -237,9 +288,9 @@ def test_file(name: str, identifiers: Dict[str,str] = {}) -> bool:
     Returns:
         bool: Whether the test passed or failed
     """
-    print(f'\n== Testing {name} ==')
+    print(f"\n== Testing {name} ==")
     proc = VHDLproc()
-    filename = os.path.dirname(__file__) + f'/tests/{name}.vhdl'
+    filename = os.path.dirname(__file__) + f"/tests/{name}.vhdl"
     passed = True
 
     try:
@@ -250,9 +301,9 @@ def test_file(name: str, identifiers: Dict[str,str] = {}) -> bool:
         passed = passed and False
 
     if passed:
-        logging.info('== Passed ==')
+        logging.info("== Passed ==")
     else:
-        logging.info('== Failed ==')
+        logging.info("== Failed ==")
 
     return passed
 
@@ -263,15 +314,30 @@ def test_all():
     Returns:
         bool: Whether the tests passed or failed
     """
-    return not (test_file('include') and test_file('and') and test_file('nest'))
+    return not (test_file("include") and test_file("and") and test_file("nest"))
+
 
 def _cli():
-    parser = argparse.ArgumentParser(description=f"VHDLproc v{__version__} - VHDL Preprocessor")
-    parser.add_argument('-i', help='Input file (Omit to read from stdin)')
-    parser.add_argument('-o', help='Output file (Omit to print to stdout)')
-    parser.add_argument('--test', action='store_true', help='Tests VHDLproc to ensure functionality')
-    parser.add_argument('-D', action="append", metavar="IDENTIFIER=value", help='Specify identifiers for conditional compilation, ex. DEBUG_LEVEL=2')
-    parser.add_argument("--log-level", default=logging.INFO, type=(lambda x: getattr(logging, x)), help = "Configure the logging level.")
+    parser = argparse.ArgumentParser(
+        description=f"VHDLproc v{__version__} - VHDL Preprocessor"
+    )
+    parser.add_argument("-i", help="Input file (Omit to read from stdin)")
+    parser.add_argument("-o", help="Output file (Omit to print to stdout)")
+    parser.add_argument(
+        "--test", action="store_true", help="Tests VHDLproc to ensure functionality"
+    )
+    parser.add_argument(
+        "-D",
+        action="append",
+        metavar="IDENTIFIER=value",
+        help="Specify identifiers for conditional compilation, ex. DEBUG_LEVEL=2",
+    )
+    parser.add_argument(
+        "--log-level",
+        default=logging.INFO,
+        type=(lambda x: getattr(logging, x)),
+        help="Configure the logging level.",
+    )
     args = parser.parse_args()
 
     logging.basicConfig(level=args.log_level, format="%(levelname)s: %(message)s")
@@ -279,13 +345,12 @@ def _cli():
     if args.test:
         retn = test_all()
         exit(retn)
-        
 
     identifiers = {}
 
     if args.D:
         for id in args.D:
-            identifiers[id.split('=')[0]] = str(id.split('=')[1])
+            identifiers[id.split("=")[0]] = str(id.split("=")[1])
 
     proc = VHDLproc()
     if args.i:
@@ -293,14 +358,14 @@ def _cli():
     else:
         code = []
         for line in sys.stdin:
-            code.append(line.rstrip('\n'))
+            code.append(line.rstrip("\n"))
         parsed_code = proc.parse(code, identifiers=identifiers)
 
-
     if args.o:
-        open(args.o, 'w+').write(parsed_code)
+        open(args.o, "w+").write(parsed_code)
     else:
         print(parsed_code)
+
 
 if __name__ == "__main__":
     _cli()
